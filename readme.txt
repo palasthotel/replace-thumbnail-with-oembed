dab7561256456a7ac8f476837c717d23ec6a623f
Kim Meyer <kim.meyer@palasthotel.de>
2019-03-03 13:46:04 +0100

    tested up to WP 5.1, updated readme.txt and some refactoring

--- readme.txt
+++ readme.txt
@@ -3,14 +3,16 @@ Contributors: palasthotel, greatestview
 Donate link: https://palasthotel.de/
 Tags: oembed, iframe, youtube, vimeo, instagram, twitter, soundcloud, thumbnail, image
 Requires at least: 4.0
-Tested up to: 4.7.3
-Stable tag: 1.2.1
+Tested up to: 5.2
+Stable tag: 1.2.2
 License: GNU General Public License v3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 Replaces the thumbnail image with the oEmbed element from the first line of the content.
 
 == Description ==
+ATTENTION for WP Version 5+: This plugin does not work with Gutenberg (no problems with Classic Editor plugin though)! Gutenberg brings its own HTML structure for oEmbed elements, which is quite difficult to target.
+
 This plugin checks, if the first line of the content is a valid oEmbed URL. If that is the case, it replaces the thumbnail image with the oEmbed element (e. g. with a YouTube video). Now you don’t have to care too much about a thumbnail image, since instead you would see the video or any other oEmbed content. You can even e. g. watch videos on your blogs front page without opening the post.
 
 Works for all WordPress contents, which have a thumbnail set and are not password protected.
@@ -23,6 +25,9 @@ The active WordPress Theme must use the_post_thumbnail() or get_the_post_thumbna
 3. You’re done! Try pasting a YouTube, Instagram or any other oEmbed URL into the first line of a post or page. Open the post and see, that the thumbnail image has been replaced by the oEmbed element.
 
 == Changelog ==
+
+= 1.2.2 =
+* WordPress 5.1 compatibility check and readme.txt update.
 
 = 1.2.1 =
 * Removed copyright section
--- replace-thumbnail-with-oembed.php
+++ replace-thumbnail-with-oembed.php
@@ -3,26 +3,28 @@
 /**
  * Plugin Name: Replace Thumbnail with oEmbed
  * Description: This plugin checks, if the first line of the content is a valid oEmbed URL. If that is the case, it replaces the thumbnail image with the oEmbed element (e. g. with a YouTube video). Now you don’t have to care too much about a thumbnail image, since instead you would see the video or any other oEmbed content. You can even e. g. watch videos on your blogs front page without opening the post. Works for all WordPress contents, which have a thumbnail set and are not password protected. The active WordPress Theme must use the_post_thumbnail() or get_the_post_thumbnail() functions for this to work!
- * Version:     1.2.1
+ * Version:     1.2.2
  * Author:      Palasthotel <rezeption@palasthotel.de> (Kim-Christian Meyer)
  * Author URI:  https://palasthotel.de
  * License:     GNU General Public License v3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- * @copyright   Copyright (c) 2017, Palasthotel GfdPmbH Berlin
+ * @copyright   Copyright (c) 2019, Palasthotel GfdPmbH Berlin
  */
 
 
+add_filter( 'post_thumbnail_html', 'replace_thumbnail_with_oembed_apply' );
 function replace_thumbnail_with_oembed_apply( $html ) {
-	if ( ! is_attachment() && has_post_thumbnail() && ! post_password_required() ) {
-		$oembed_html = replace_thumbnail_with_oembed_replace_first_line_of_content_with_oembed();
-		if ( $oembed_html !== false ) {
-			return $oembed_html;
-		}
+	if ( is_attachment() || ! has_post_thumbnail() || post_password_required() ) {
+		return $html;
 	}
 
-	return $html;
+	$oembed_html = replace_thumbnail_with_oembed_replace_first_line_of_content_with_oembed();
+	if ( $oembed_html === false ) {
+		return $html;
+	}
+
+	return $oembed_html;
 }
-add_filter( 'post_thumbnail_html', 'replace_thumbnail_with_oembed_apply' );
 
 
 /**
